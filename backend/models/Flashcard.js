09e25e96b1fc61da4c37d11e7cf77a14179429d2
alifconstantine <alifconstantine1@gmail.com>
2026-01-07 21:45:18 +0700

    Build Document and Flashcard APIs

--- backend/models/Flashcard.js
+++ backend/models/Flashcard.js
@@ -28,7 +28,7 @@ const flashcardSchema = new mongoose.Schema({
                 type: Number,
                 default: 0
             },
-            isStarted: {
+            isStarred: {
                 type: Boolean,
                 default: false
             }
